2cf9384194cb2e0ce105a24e80e2156dc2992e23
Fran Ferri <franferri@gmail.com>
2020-02-23 23:39:23 +0000

    self_install script

---
+++ self_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt install python3-venv git sshpass i2c-tools python-smbus joystick xboxdrv -y
+
+grep -qxF 'options bluetooth disable_ertm=Y' /etc/modprobe.d/bluetooth.conf || echo 'options bluetooth disable_ertm=Y' | sudo tee -a /etc/modprobe.d/bluetooth.conf
+cat /etc/modprobe.d/bluetooth.conf
+
+cd ~
+git clone https://gitlab.com/custom_robots/spotmicroai/basic-runtime.git spotmicroai
+cd spotmicroai
+
+chmod +x *.sh
+
+python3 -m venv venv --clear
+source venv/bin/activate
+
+curl https://bootstrap.pypa.io/get-pip.py | python
+
+python3 -m pip install --upgrade pip setuptools smbus jmespath adafruit-circuitpython-motor adafruit-circuitpython-pca9685 RPi.GPIO inputs
+
