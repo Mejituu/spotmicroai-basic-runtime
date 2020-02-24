1f54dc29c1a930d22043ba28be1892d7631667b3
Fran <franferri@gmail.com>
2020-02-24 14:05:56 +0000

    lcd screen test

--- run_integration_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=.
-
-python3 integration_tests/test_lcd_screen.py
-
-# include more scripts here...
-# python3 integration_tests/test_lcd_screen.py
---
+++ run_integration_test_lcd_screen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+~/spotmicroai/venv/bin/python3 ~/spotmicroai/integration_tests/test_lcd_screen/test_lcd_screen.py
+
