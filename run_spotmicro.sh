7fce7eab566c22d3abd383d7827b7e375a548b9b
Fran <franferri@gmail.com>
2020-02-24 19:12:49 +0000

    old script updated

--- run_spotmicro.sh
+++ run_spotmicro.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd ~/spotmicroai
 export PYTHONPATH=.
 
-/home/pi/projects/raspberrypi/5.\ Configure\ and\ activate\ your\ SpotMicro/spotmicropi_runtime/venv/bin/python3 spotmicro/main.py
+venv/bin/python3 spotmicro/main.py
