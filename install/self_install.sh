3f762907ef7dcc55c986265c539b8fc11eaf5bdb
Fran Ferri <franferri@gmail.com>
2020-02-25 20:07:54 +0000

    Update self_install.sh

--- install/self_install.sh
+++ install/self_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt update -y
+
+sudo apt dist-upgrade -y
+
 sudo apt install git python3-venv sshpass i2c-tools python-smbus joystick xboxdrv -y
 
 grep -qxF 'options bluetooth disable_ertm=Y' /etc/modprobe.d/bluetooth.conf || echo 'options bluetooth disable_ertm=Y' | sudo tee -a /etc/modprobe.d/bluetooth.conf
