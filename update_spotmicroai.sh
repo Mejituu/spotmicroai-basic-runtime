21c8b0d872ae5d7e27efe09b32573ea73f5ffdc6
franferri <franferri@gmail.com>
2020-03-07 12:04:39 +0000

    updated update scripts to install the necesary python packages

--- update_spotmicroai.sh
+++ update_spotmicroai.sh
@@ -8,3 +8,12 @@ git clean -df
 git checkout master
 git pull
 
+chmod +x *.sh
+
+python3 -m venv venv --clear
+source venv/bin/activate
+
+curl https://bootstrap.pypa.io/get-pip.py | python
+
+python3 -m pip install --upgrade pip setuptools smbus jmespath adafruit-circuitpython-motor adafruit-circuitpython-pca9685 RPi.GPIO inputs pick
+
--- update_spotmicroai_from_development.sh
+++ update_spotmicroai_from_development.sh
@@ -8,3 +8,11 @@ git clean -df
 git checkout development
 git pull
 
+chmod +x *.sh
+
+python3 -m venv venv --clear
+source venv/bin/activate
+
+curl https://bootstrap.pypa.io/get-pip.py | python
+
+python3 -m pip install --upgrade pip setuptools smbus jmespath adafruit-circuitpython-motor adafruit-circuitpython-pca9685 RPi.GPIO inputs pick
