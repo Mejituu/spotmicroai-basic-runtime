884edb04b6baecb32c1df35e6ecb909197922b73
Fran <franferri@gmail.com>
2020-03-13 18:46:12 +0000

    testing website

--- utilities/activate.sh
+++ utilities/activate.sh
@@ -7,4 +7,4 @@ source venv/bin/activate
 
 curl https://bootstrap.pypa.io/get-pip.py | python
 
-python3 -m pip install --upgrade pip setuptools jmespath adafruit-circuitpython-motor adafruit-circuitpython-pca9685 inputs smbus RPi.GPIO inputs
+python3 -m pip install --upgrade pip setuptools jmespath adafruit-circuitpython-motor adafruit-circuitpython-pca9685 inputs smbus RPi.GPIO inputs websockets flask
