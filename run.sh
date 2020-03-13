4c23df3eecb2406f1b4e2af5ebe7351e2b263619
Fran <franferri@gmail.com>
2020-03-13 17:34:55 +0000

    run.sh prevents to launch many instances, just one allowed

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pidfile="$HOME/spotmicroai/.lock"
-if [ -f "$pidfile" ] && kill -0 $(cat "$pidfile") 2>/dev/null; then
+if [ -f "$pidfile" ] && kill -0 "$(cat "$pidfile")" 2>/dev/null; then
     echo still running
     exit 1
 fi
