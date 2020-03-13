f4cf0f1460d5bed78db94fb44bda737931153113
Fran <franferri@gmail.com>
2020-03-13 18:59:28 +0000

    activate x86 upgraded

--- utilities/development/activate_x86.sh
+++ utilities/development/activate_x86.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ../..
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$SCRIPT_DIRECTORY/../.." || exit
 
 python3 -m venv venv --clear
 source venv/bin/activate
