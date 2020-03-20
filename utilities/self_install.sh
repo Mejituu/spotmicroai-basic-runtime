57519f347afe63bd737e163212f7edca9524b14c
Fran Ferri <franferri@gmail.com>
2020-03-20 19:42:43 +0000

    Update self_install.sh

--- utilities/self_install.sh
+++ utilities/self_install.sh
@@ -14,6 +14,7 @@ cat /etc/modprobe.d/bluetooth.conf
 cd ~ || exit
 git clone https://gitlab.com/custom_robots/spotmicroai/basic-runtime.git spotmicroai
 cd spotmicroai || exit
+git checkout ba031e5fad88fbcd751b23df0d87d6e76496068c
 
 find . -type f -iname "*.sh" -exec chmod +x {} \;
 
